18ca07992d9fb06d25dd5b13e33886781906e968
Seshat <seshat.anelica@gmail.com>
2015-06-23 15:37:58 -0500

    declaraciones de funciones

---
+++ calcualdora/src/calcualdora/Calcualdora.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calcualdora;
+
+/**
+ *
+ * @author Angelica
+ */
+public class Calcualdora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public  float suma( float a, float b){
+            
+     return 0;
+    }
+     public  float resta( float a, float b){
+            
+     return 0;
+    }
+      public  float Multiplicacion( float a, float b){
+            
+     return 0;
+    }
+       public  float division( float a, float b){
+            
+     return 0;
+    }
+
+}
